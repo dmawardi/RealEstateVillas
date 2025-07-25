a84090d0e07788c7d06f91033c2ac72ce69ce903
Dannar Mawardi <dMawardi@gmail.com>
2025-07-25 11:55:09 +0800

    Cleaned up base layout to use Breadcrumbs component

--- resources/js/layouts/BaseLayout.vue
+++ resources/js/layouts/BaseLayout.vue
@@ -2,6 +2,7 @@
 // Props breadcrumbs
 import FlashMessages from '@/components/FlashMessages.vue';
 import type { BreadcrumbItemType } from '@/types';
+import Breadcrumbs from '@/components/Breadcrumbs.vue';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -12,23 +13,12 @@ const { breadcrumbs } =withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <!-- Breadcrumbs bar -->
-    <nav class="bg-white dark:bg-gray-800 shadow-sm">
-        <FlashMessages />
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
-            <ol class="list-reset flex text-gray-700 dark:text-gray-300">
-                <li>
-                    <a href="/" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">Home</a>
-                </li>
-                <li class="mx-2">/</li>
-                <li v-for="(breadcrumb, index) in breadcrumbs" :key="index">
-                    <a :href="breadcrumb.href" class="text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300">
-                        {{ breadcrumb.title }}
-                    </a>
-                    <span v-if="index < breadcrumbs.length - 1" class="mx-2">/</span>
-                </li>
-            </ol>
+    <FlashMessages />
+    <main>
+        <!-- Breadcrumbs bar -->
+        <div v-if="breadcrumbs.length > 0" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
+            <Breadcrumbs :breadcrumbs="breadcrumbs" />
         </div>
-    </nav>
-    <slot />
+        <slot />
+    </main>
 </template>
