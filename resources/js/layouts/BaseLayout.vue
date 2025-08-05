f0e1f2229ef6688b8c50210f8b0d3f9daee8ccc8
Dannar Mawardi <dMawardi@gmail.com>
2025-08-05 12:09:47 +0800

    Fixed breadcrumbs in base layout to not use withDefaults()

--- resources/js/layouts/BaseLayout.vue
+++ resources/js/layouts/BaseLayout.vue
@@ -7,16 +7,14 @@ import Breadcrumbs from '@/components/Breadcrumbs.vue';
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
 }
-const { breadcrumbs } =withDefaults(defineProps<Props>(), {
-    breadcrumbs: () => [],
-});
+const { breadcrumbs } =defineProps<Props>();
 </script>
 
 <template>
     <FlashMessages />
     <!-- Breadcrumbs bar -->
-    <div v-if="breadcrumbs.length > 0" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
-        <Breadcrumbs :breadcrumbs="breadcrumbs" />
+    <div v-if="(breadcrumbs?.length ?? 0) > 0" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
+        <Breadcrumbs :breadcrumbs="breadcrumbs ?? []" />
     </div>
     <main>
         <slot />
