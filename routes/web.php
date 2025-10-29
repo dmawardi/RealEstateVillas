981c556180bc01da287fb496058c3d9afe4d4e72
Dannar Mawardi <dMawardi@gmail.com>
2025-10-29 20:01:22 +0800

    Added EnsureUserIsAdmin middleware and added to admin routes

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::get('dashboard', function () {
 // ADMIN ROUTES
 // 
 // Properties
-Route::get('admin/properties', [AdminPropertyController::class, 'index'])->middleware(['auth', 'verified'])->name('admin.properties.index');
-Route::get('admin/properties/create', [AdminPropertyController::class, 'create'])->middleware(['auth', 'verified'])->name('admin.properties.create');
-Route::get('admin/properties/{property}', [AdminPropertyController::class, 'show'])->middleware(['auth', 'verified'])->name('admin.properties.show');
-Route::post('admin/properties', [AdminPropertyController::class, 'store'])->middleware(['auth', 'verified'])->name('admin.properties.store');
-Route::get('admin/properties/{property}/edit', [AdminPropertyController::class, 'edit'])->middleware(['auth', 'verified'])->name('admin.properties.edit');
-Route::put('admin/properties/{property}', [AdminPropertyController::class, 'update'])->middleware(['auth', 'verified'])->name('admin.properties.update');
+Route::get('admin/properties', [AdminPropertyController::class, 'index'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.index');
+Route::get('admin/properties/create', [AdminPropertyController::class, 'create'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.create');
+Route::get('admin/properties/{property}', [AdminPropertyController::class, 'show'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.show');
+Route::post('admin/properties', [AdminPropertyController::class, 'store'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.store');
+Route::get('admin/properties/{property}/edit', [AdminPropertyController::class, 'edit'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.edit');
+Route::put('admin/properties/{property}', [AdminPropertyController::class, 'update'])->middleware(['auth', 'verified', 'admin'])->name('admin.properties.update');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
