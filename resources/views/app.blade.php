4746d13ca206cdcd62271dbe730e7d0f6e81c2cb
Dannar Mawardi <dMawardi@gmail.com>
2026-01-13 20:33:23 +0800

    Added script josn tags for Google Search

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -36,12 +36,41 @@
         <link rel="icon" type="image/png" href="/logo-touch-icon.png" sizes="180x180">
         <link rel="apple-touch-icon" href="/logo-touch-icon.png">
 
+        
+
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
         @inertiaHead
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@type": "Organization",
+                "@id": "https://balivillaspot.com/#organization",
+                "name": "Bali Villa Spot",
+                "url": "https://balivillaspot.com/",
+                "logo": "https://balivillaspot.com/logo.png",
+                "sameAs": [
+                    "https://www.instagram.com/balivillaspot",
+                    "https://www.facebook.com/balivillaspot"
+                ]
+            }
+        </script>
+
+        <script type="application/ld+json">
+            {
+                "@context": "https://schema.org",
+                "@type": "WebSite",
+                "@id": "https://balivillaspot.com/#website",
+                "url": "https://balivillaspot.com/",
+                "name": "Bali Villa Spot",
+                "publisher": {
+                    "@id": "https://balivillaspot.com/#organization"
+                }
+            }
+        </script>
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-XS9NW8Z5P8"></script>
         <script>
